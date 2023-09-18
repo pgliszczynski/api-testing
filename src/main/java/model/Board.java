52a238aba4b4436d548d69adfa3a9edda6f0db5f
Piotr Gliszczynski <pgliszczynski@griddynamics.com>
2023-09-18 13:45:52 +0200

    Add model classes - User and Board

---
+++ src/main/java/model/Board.java
@@ -0,0 +1,26 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Board {
+    @JsonProperty
+    private String id;
+    @JsonProperty
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,46 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+    @JsonProperty
+    private String id;
+    @JsonProperty
+    private String fullName;
+    @JsonProperty
+    private String username;
+    @JsonProperty
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
