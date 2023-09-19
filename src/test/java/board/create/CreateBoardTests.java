80d7cc48404302fecb9c81d001999897e78cbda9
Piotr Gliszczynski <pgliszczynski@griddynamics.com>
2023-09-19 15:49:10 +0200

    Add tests for creating boards

---
+++ src/test/java/board/create/CreateBoardTests.java
@@ -0,0 +1,39 @@
+package board.create;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.ApacheHttpClientTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.testng.Assert.assertTrue;
+
+public class CreateBoardTests extends ApacheHttpClientTest {
+
+    @Test
+    void shouldCreateNewBoardTestNG() {
+        //Given
+
+        //When
+
+        //Then
+        assertTrue(boardResponseValidator.isExpectedResponse(actualBoardResponse));
+    }
+
+    @Test
+    void shouldCreateNewBoardAssertJ() {
+        //Given
+
+        //When
+
+        //Then
+        assertThat(boardResponseValidator.isExpectedResponse(actualBoardResponse))
+                .isTrue();
+    }
+
+    @BeforeMethod
+    @Override
+    public void sendRequest() {
+        actualBoardResponse = httpClient.postNewBoard();
+        updateExpectedBoardId();
+    }
+}
